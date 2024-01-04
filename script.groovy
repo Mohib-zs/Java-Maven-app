33e13ed56d4a7e233de494307b51505ca5a6fe39
Muhammad Mohib Shaikh <mohibzahid97@gmail.com>
2024-01-04 10:57:13 +0000

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -31,8 +31,10 @@ def commitVersion() {
     echo 'commiting version bump...'
     withCredentials([usernamePassword(credentialsId: 'gitlab-credentials', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
         sh "git remote set-url origin https://${USER}:${PASS}@gitlab.com/Mohib-zs/Java-Maven-app.git"
-        sh 'git config --global user.email "mohibzahid97@gmail.com"'
-        sh 'git config --global user.name "Mohib Shaikh"'
+        sh 'git config --global user.email "jenkins@example.com"'
+        sh 'git config --global user.name "jenkins"'
+        sh 'git status'
+        sh 'git config --list'
         sh 'git add .'
         sh 'git commit -m "ci: version bump"'
         sh 'git push origin HEAD:jenkins-job'
