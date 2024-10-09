be6a52a2d94136d02d809d0ce90c899a7fb95506
Muhammad Mohib Shaikh <109289989+Mohib-zs@users.noreply.github.com>
2024-10-09 22:01:13 +0500

    Update startup-script.sh

--- terraform/startup-script.sh
+++ terraform/startup-script.sh
@@ -5,5 +5,5 @@ sudo systemctl start docker
 sudo usermod -aG docker azureuser
 
 #Installing Docker Compose
-sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/v2.29.7/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
